dee6c30d5b207ca9608a92a0a9c3515a983d4506
NakanishiBacon <nakanishiiakonatetsu@gmail.com>
2025-05-05 18:15:52 -0700

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -11,7 +11,7 @@ from scipy.stats import skew, kurtosis
 import plotly.graph_objects as go
 
 # Set layout, title, and page icon for the Streamlit app
-st.set_page_config(layout="wide", page_title="Sentiment Dashboard", page_icon="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/CAHSRA_Logo.svg/640px-CAHSRA_Logo.svg.png")
+st.set_page_config(layout="wide", page_title="Sentiment Dashboard", page_icon="https://styles.redditmedia.com/t5_3iapt/styles/communityIcon_4iqd676dihh51.png")
 
 # ========================
 # File Mappings by Source
