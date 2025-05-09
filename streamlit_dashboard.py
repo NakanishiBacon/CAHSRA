59021bed6f73f0e40896b86669c1d4af7a988259
NakanishiBacon <nakanishiiakonatetsu@gmail.com>
2025-05-08 21:44:27 -0700

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -270,7 +270,7 @@ else:
 
 logo = logo_image_map.get(source)
 post_summary += f"<strong style='font-size:1.2rem;'><img src='{logo}' width='22' style='vertical-align:middle; margin-right:6px;'> {source} Total Posts: {len(filtered_df):,}</strong>" if logo else f"<strong style='font-size:1.2rem;'>📊 {source} Total Posts: {len(filtered_df):,}</strong>"
-post_summary += "<ul style='margin: 0; padding: 0 0 0 1.2em; list-style-type: none; font-size: 1.2rem;'>""
+post_summary += "<ul style='margin: 0; padding: 0 0 0 1.2em; list-style-type: none; font-size: 1.2rem;'>"
 
 if source == "Combined":
     
